9e13e51adc599e2621a5138a1894e47a775d1c96
HOOOO98 <loading2us@gmail.com>
2023-07-18 21:40:40 +0900

    First commit

---
+++ content.js
@@ -0,0 +1,13 @@
+// 광고 스킵 버튼을 클릭하는 함수
+function clickSkipButton() {
+  const skipButton = document.querySelector(".ytp-ad-skip-button.ytp-button");
+  if (skipButton) {
+    skipButton.click();
+  }
+}
+
+// 페이지가 로드되면 광고 스킵 버튼을 5초마다 찾아서 클릭합니다.
+window.addEventListener("load", () => {
+  setInterval(clickSkipButton, 5000);
+});
+
